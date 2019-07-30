22051d7a75ceb3b01188d1d03c992969e4494a21
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-07-30 11:54:55 -0700

    initial commit after restructure and started implementing store

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Router>
+    <Provider store={store} />
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
