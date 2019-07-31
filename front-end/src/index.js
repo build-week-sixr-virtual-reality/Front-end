a559f256d958623ac9339a3bc466b2b1e030bf7c
Seth Nadu <seth.nadu@gmail.com>
2019-07-30 20:29:28 -0400

    projectfunding

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import thunk from "redux-thunk";
