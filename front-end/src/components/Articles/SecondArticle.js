90a58a036aae3e72cc1aa0963fad9f21966ccaa2
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-08-01 21:03:37 -0400

    Resources page complete

--- front-end/src/components/Articles/SecondArticle.js
+++ front-end/src/components/Articles/SecondArticle.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import Styled from "styled-components";
+import article2 from '../../assets/ArticleImages/article2.jpg'
+import article22 from '../../assets/ArticleImages/article22.jpg'
+
+
+
 
 const TitleContainer = Styled.div `
     margin: 30px 30px 0px 30px;
@@ -32,7 +37,7 @@ const Paragraph = Styled.p `
 
 const ArticleContainer = Styled.div `
     background-color: #ECF9FE;
-    height: 670px
+    height: 100%
     margin: 0px 0px 0px 30px
     width: 809px
 
@@ -47,6 +52,7 @@ const BigBoxContainer = Styled.div `
 
     
 `
+
 const ImgContainer1 = Styled.img `
     width: 501px;
     height: 334.04px;    
@@ -59,6 +65,57 @@ const HeaderContainers1 = Styled.h1 `
 const TextContainers = Styled.p `
     text-align:left
     margin-right:50px
+
+`
+const BoxHeader = Styled.h2 `
+
+width: 305px;
+height: 30px;
+left: 1071px;
+top: 278px;
+
+font-family: Open Sans;
+font-style: normal;
+font-weight: bold;
+font-size: 22px;
+line-height: 30px;
+
+
+
+color: #FECA3B;
+
+`
+
+const BoxPara = Styled.p `
+width: 280px;
+height: 88px;
+left: 900px;
+top: 318px;
+
+font-family: Open Sans;
+font-style: normal;
+font-weight: normal;
+font-size: 16px;
+line-height: 22px;
+
+color: #FECA3B;
+`
+
+const AnchorBox = Styled.a`
+width: 305px;
+height: 22px;
+left: 800px;
+top: 416px;
+
+font-family: Open Sans;
+font-style: normal;
+font-weight: bold;
+font-size: 16px;
+line-height: 22px;
+
+
+
+color: #FECA3B;
 
 `
 
@@ -76,7 +133,7 @@ export default function SecondArticle(){
                 <ArticleContainer>
                     <HeaderContainers1>Lorem ipsum dolor sit.</HeaderContainers1>
                     <TextContainers>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</TextContainers>
-                    {/* <ImgContainer1 src ={article1} alt = "VR Things" /> */}
+                    <ImgContainer1 src ={article2} alt = "VR Things" />
                     <HeaderContainers1>Lorem ipsum dolor sit.</HeaderContainers1>
                     <TextContainers>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</TextContainers>
                     <TextContainers>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</TextContainers>
@@ -84,21 +141,21 @@ export default function SecondArticle(){
                     <TextContainers>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</TextContainers>
                     <HeaderContainers1>Lorem ipsum dolor sit. </HeaderContainers1>
                     <TextContainers>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</TextContainers>
-                    {/* <ImgContainer1 src ={article12} alt ="VR Things" /> */}
+                    <ImgContainer1 src ={article22} alt ="VR Things" />
                     <HeaderContainers1>Lorem ipsum dolor sit. </HeaderContainers1>
                     <TextContainers>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</TextContainers>
 
                 </ArticleContainer>
-                
-                
-                
-            </div>
-            <div>
-                <BigBoxContainer>
 
+                <BigBoxContainer>
+                    <BoxHeader>Related Resources:</BoxHeader>
+                    <BoxPara>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</BoxPara>
+                    <AnchorBox href = 'https://google.com'>www.google.com </AnchorBox>
                 </BigBoxContainer>
                 
+                
             </div>
+            
         </div>
     )
     
